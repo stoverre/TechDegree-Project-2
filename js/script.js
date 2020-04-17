5b4a9436b8fc61f8e716eabb6b7bd8705cd725f3
Richard Stover <stoverre@gmail.com>
2020-04-17 13:07:45 -0400

    updated showPage()
    added a returned array of displayed students from the function

--- js/script.js
+++ js/script.js
@@ -51,21 +51,28 @@ for(let index=10; index<20; index+=1){
  * 
  * @param {array} list - list of students
  * @param {integer} page - the page to display
- * 
+ * @return {array} - return a list of the displayed students
  */
 function showPage(list, page){
+   //start and end indexes of the page to be display
+   //allows the function to be flexible for all list
+   //lengths, page sizes, and page to be displayed
    let startIndex = (page*showPerPage)-showPerPage;
-   let endIndex = page*showPerPage
+   let endIndex = page*showPerPage;
+   let displayedList = [];
    
    //loop through the list of students. show students on the target
    //page and hide all others. 
    for(let i=0; i<studentList.length; i+=1){
       if(i>=startIndex && i<endIndex){
          studentList[i].style.display = '';
+         //add each displayed student to the list to be returned later
+         displayedList.push(studentList[i]);
       } else {
          studentList[i].style.display = 'none';
       }
    }
+   return displayedList;
 }
 
 

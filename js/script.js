a134e3d98ce7c97bb00f246763e8e6ff5031eba2
Richard Stover <stoverre@gmail.com>
2020-04-18 00:24:13 -0400

    comments update and some refractoring

--- js/script.js
+++ js/script.js
@@ -12,17 +12,13 @@ let showPerPage = 10;
 
 /**
  * @function showPage - takes in a list of students and a page # and hides 
- *                      all students except for that page
+ *                      all students on the list except for that page
  * 
  * @param {array} list - list of students
  * @param {integer} page - the page to display
- * @return {array} - return a list of the displayed students. This should help 
- *                   with the extra credit
  */
 function showPage(list, page){
-   //start and end indexes of the page to be display
-   //allows the function to be flexible for all list
-   //lengths, page sizes, and page to be displayed
+   //start and end list indexes of the page to be displayed
    let startIndex = (page*showPerPage)-showPerPage;
    let endIndex = page*showPerPage;
    
@@ -34,10 +30,9 @@ function showPage(list, page){
          studentList[i].style.display = 'none'
       }      
    }
-   //loop through the passed in list of students. show students on the target
-   //page and hide all others. 
+   //loop through the passed in list of students. show only students on the target
+   //page.
    for(let i=0; i<list.length; i+=1){
-      
       if(i>=startIndex && i<endIndex){
          list[i].style.display = '';
       } else{
@@ -50,22 +45,22 @@ function showPage(list, page){
  * @function createPageLinks - Creates a <div> and appends an <li> with an embedded
  *                             <a> for each display page. Then updates the DOM with 
  *                             the new <div>
- * 
  * @param {array} list - list of students
  */
 
 function appendPageLinks(list){
    //get a reference to the master html list
    let ul = document.querySelector('ul')
-   //create the <div> to be added to the DOM.
+   
+   //create the <div> and new child <ul> to be added to the DOM.
    let div = document.createElement('div')
    div.className = 'pagination'
+   let newUl = document.createElement('ul')
+   
    //determine how many total pages are necessary
    let pageCount = Math.floor(list.length/showPerPage)+1
-   let newUl = document.createElement('ul')
 
-   //loop through the passed in list and create an <li>. with an embedded <a>
-   //for each page
+   //create an <li>. with an embedded <a> for each page
    for(let i=0; i<pageCount; i+=1){
       let a = document.createElement('a')
       let li = document.createElement('li')
@@ -78,11 +73,11 @@ function appendPageLinks(list){
       li.appendChild(a)
       newUl.appendChild(li)
    }
-   //check if there is already a <div> for page links and replace it
    
-   //ad the newly created <ul> to the newly created <div>
+   //add the newly assembled <ul> to the newly created <div>
    div.appendChild(newUl)
-   //add the newly created <div> to the DOM as a child of the main <div>
+
+   //add the newly created <div> to the DOM as the last child of the main <div>
    //or replace it if a page links <div> already exists
    if(ul.parentNode.lastElementChild.className === 'pagination'){
       ul.parentNode.replaceChild(div,ul.parentNode.lastElementChild)
@@ -90,25 +85,21 @@ function appendPageLinks(list){
       ul.parentNode.appendChild(div)
    }
 
-
    //add an event listener to the new <div>
    div.lastElementChild.addEventListener('click', (event) => {
-   //make sure we clicked on a page <a>
-   if(event.target.tagName === 'A'){
-      let pageNum = parseInt(event.target.textContent)
-      //loop through all the page <li>s 
-      for(let i=0; i<newUl.children.length; i+=1){
-         if(i===pageNum-1){
-            //set the class = "active" for the clicked page <a>
-            newUl.children[i].firstElementChild.className = 'active';
-            showPage(list, pageNum)
-         }else{
-            //set the class = "" for all other page <a>
+      //make sure we clicked on a page <a>
+      if(event.target.tagName === 'A'){
+         //get the page number that was clicked into integer form and show that page
+         let pageNum = parseInt(event.target.textContent)
+         showPage(list, pageNum)
+         //loop through all the page <li>s and set to inactive 
+         for(let i=0; i<newUl.children.length; i+=1){
             newUl.children[i].firstElementChild.className = '';
-         }
+         }   
+         //set pageNum page <a> to active
+         newUl.children[pageNum-1].firstElementChild.className = 'active'
       }
-   }
-})
+   })
 }
 
 /**
@@ -142,8 +133,29 @@ function searchBar(list){
 
     //nest the new button, input, and div into the header
     div.appendChild(searchInput)
-    div.appendChild(searchButton)
+    /**
+     * The button was intentionally removed once the live search function was
+     * implemented. But all the correct logic remains in the code.
+     */
+    //div.appendChild(searchButton)
     headerDiv.appendChild(div)
+
+   /**
+    * @function prepareToValidate - DRY function to handle some conditional house 
+    *                               cleaning when the 2nd-nth search events occur
+    * @param {} - none
+    * @return {} - none
+    */
+   function prepareToValidate(){
+      //if a no result or error message had been displayed (messageAdded)
+       //remove the last list item from the DOM
+       if (messageAdded){
+          ul.removeChild(ul.lastElementChild)
+          messageAdded = false;
+       }
+       searchResults = []
+       validateInput(searchInput.value)
+    }
 
    /**
     * @function validateInput - determines if the search input contains any invalid 
@@ -154,6 +166,7 @@ function searchBar(list){
     *                     character as defined in invalidChar[]
     */
    function validateInput(input){
+      //initialize the flag and compare each input char to the invalid chars
       let inputIsValid = true;
       let invalidChars = [0,1,2,3,4,5,6,7,8,9,"/","*","-","+",".","?","<",">",
                            "`","~","!","@","#","$","%","^","&",","," "]
@@ -164,6 +177,8 @@ function searchBar(list){
             }
          }
       }
+      //if the flag was set false, the input is not valid and we need to display a 
+      //message. Else check the input against the list
       if (!inputIsValid){
          searchResults.push(createErrorMessage('The Input May Only Contain Letters'))
          showPage(searchResults, 1)
@@ -175,7 +190,9 @@ function searchBar(list){
 
    /**
     * @function doesNameMatch - compares a passed in string and compares it to the
-    *                           entire list of students.
+    *                           entire list of students. Update the searchResult list
+    *                           with any students that match the input. Create a
+    *                           message if no results are found
     * @param {string} input - a passed in string to search for
     * @return {} - none
     */
@@ -202,10 +219,9 @@ function searchBar(list){
          searchResults.push(createErrorMessage('No Results Found'))
       }
 
-      //update the page
+      //update the page with the search results
       showPage(searchResults, 1)
       appendPageLinks(searchResults)
-
    }
 
    /**
@@ -216,15 +232,17 @@ function searchBar(list){
     * @return {object} li - returns the newly created <li>
     */
    function createErrorMessage(reason){
-      //create the new parent <li>, child <div>, and <div> child <h3>
+      //create the new parent <li> and child elements
       let li = document.createElement('li')
-      li.className = "student-item"
+      li.className = "student-item cf"
+      li.style.borderBottom = '1px solid #eaeaea'
+      li.style.padding = '0 0 38px 0'
       let div = document.createElement('div')
       div.className = "student-details"
       let h3 = document.createElement('h3')
       h3.textContent = [reason]
       
-      //nest the new <h3>, <div>, and <li
+      //nest the new <h3>, <div>, and <li>
       div.appendChild(h3)
       li.appendChild(div)
       ul.appendChild(li)
@@ -232,24 +250,17 @@ function searchBar(list){
 
       return li
    }
-   //listen for a mouse click on the search <button> then run the name search
+
+   //listen for a mouse click on the search <button> and prepare to validate 
    div.addEventListener('click', (event) => {
       if(event.target.tagName === 'BUTTON'){
-         doesNameMatch(searchInput.value)
+         prepareToValidate()
       }
    })
 
-   //listen for a keyboard key release then run the name search
-   div.addEventListener('keyup', (event) => {
-      //if a no result or error message had been displayed (messageAdded)
-      //remove the last list item from the DOM
-      if (messageAdded){
-         ul.removeChild(ul.lastElementChild)
-         messageAdded = false;
-      }
-      searchResults = []
-      validateInput(searchInput.value)
-   })
+   //listen for a keyboard key release and prepare to validate
+   div.addEventListener('keyup', () => {prepareToValidate()})
+
 }
 
 showPage(studentList, 1)
